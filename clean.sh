0e5e1a02c6387ba6c30b983b390711376a81af2f
Eric Wyles <23637493+ericwyles@users.noreply.github.com>
2024-06-27 11:27:05 -0500

    initial commit

---
+++ clean.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+rm buildkey.pem
+rm buildpublic.pem
+rm policy.tmp.yaml
+rm policy.json
+rm policykey.pem
+rm policypublic.pem
+rm policy.signed.json
+rm hello.txt
+rm build-attestation.json
